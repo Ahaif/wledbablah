3f221ae75acfc1014b405baedd23925640c0d946
Ahaif <haifouli.abdelmoughite@gmail.com>
2024-04-02 00:32:35 +0000

    gas_cost_dynamic

--- src/dexInteractions.ts
+++ src/dexInteractions.ts
@@ -8,6 +8,15 @@ import { DEX_IDENTIFIERS } from './constants';
 dotenv.config();
 
 const provider = new ethers.providers.JsonRpcProvider(process.env.INFURA_URL);
+// Assuming you're getting the private key like this:
+const privateKey = process.env.PRIVATE_KEY;
+
+// Validate privateKey is not undefined before using it to create the Wallet instance
+if (!privateKey) {
+  throw new Error("PRIVATE_KEY environment variable is not set.");
+}
+
+const wallet = new ethers.Wallet(privateKey, provider); 
 const uniswapRouterContract = new ethers.Contract(DEX_IDENTIFIERS.UNISWAP, UniswapRouterABI.result, provider);
 const SushiswapRouterContract = new ethers.Contract(DEX_IDENTIFIERS.SUSHISWAP, SushiswapRouterAbi.result, provider);
 
@@ -63,52 +72,104 @@ export async function fetch_LiquiditySushiswap(tokenA:string,tokenB: string) {
     console.log(`Liquidity for token in sushiswap ${tokenB}: ${ethers.utils.formatEther(amountsOut[1])}`);
     return amountsOut[1];
  
-
-    // Logic for comparing prices for arbitrage goes here
-    // ...
   } catch (error : any) {
     console.error(`Error fetching liquidity: ${error.message}`);
   }
 
 }
 
-export async function calculateArbitrageProfit(amountOutUniswap: BigNumber, amountOutSushiswap: BigNumber) {
+
+async function getCurrentGasPrice(){
+  try {
+      const gasPrice = await provider.getGasPrice();
+      console.log(`Current gas price: ${ethers.utils.formatUnits(gasPrice, 'gwei')} gwei`);
+      return gasPrice;
+  } catch (error) {
+      console.error('Error fetching current gas price:', error);
+      throw error;
+  }
+}
+
+
+export async function estimateGasLimitForTokenSwap(
+  dexRouterContract : ethers.Contract,
+  tokenInAddress: string,
+  tokenOutAddress: string,
+  tokenInAmount: BigNumber,
+  userAddress: string,
+) {
+  try{
+    let now = new Date
+// Uniswap requires a deadline for the swap. 30 minutes from now expressed as milliseconds since epoch 
+const deadline = now.setTime(now.getTime() + (30 * 60 * 1000)) 
+const gasLimit = await dexRouterContract.estimateGas.swapExactTokensForTokens(
+  tokenInAmount,
+  0,
+  [
+      tokenInAddress,
+      tokenOutAddress
+  ],
+  userAddress,
+  deadline,
+  {
+      from: userAddress
+  }
+)
+return gasLimit.toString()
+
+  }catch(e : any){
+    throw new Error(`Error estimating gas limit for token swap: ${e.message}`);
+  }
+
+}
+
+
+
+
+
+export async function calculateArbitrageProfit(amountOutUniswap: BigNumber, amountOutSushiswap: BigNumber, tokenA: string, tokenB: string): Promise<void> {
 
     const amountOutUniswapBigNumber = ethers.BigNumber.from(amountOutUniswap.toString());
     const amountOutSushiswapBigNumber = ethers.BigNumber.from(amountOutSushiswap.toString());
 
 
-    // Define slippage tolerance as 1%, for example
+    
     try{   
+       
         const slippageTolerance = 0.01; // 1%
-        const estimatedGasFeeUniswap = ethers.utils.parseEther("0.01"); // Example: 0.01 ETH
-        const estimatedGasFeeSushiswap = ethers.utils.parseEther("0.01"); // Example: 0.01 ETH
+
+        const gasCostUniswap = await estimateGasLimitForTokenSwap(uniswapRouterContract, tokenA, tokenB, ethers.utils.parseEther("10"), wallet.address);
+        const gasCostSushiswap = await estimateGasLimitForTokenSwap(uniswapRouterContract, tokenA, tokenB, ethers.utils.parseEther("10"), wallet.address);
+        console.log(`Gas cost for Uniswap: ${ethers.utils.formatEther(gasCostUniswap)}`);
+        console.log(`Gas cost for Sushiswap: ${ethers.utils.formatEther(gasCostSushiswap)}`);
+    
 
 
-        const slippageFactorUniswap = amountOutUniswapBigNumber.mul(100 - (slippageTolerance * 100)).div(100);
-        const slippageFactorSushiswap = amountOutSushiswapBigNumber.mul(100 - (slippageTolerance * 100)).div(100);
+    //     const slippageFactorUniswap = amountOutUniswapBigNumber.mul(100 - (slippageTolerance * 100)).div(100);
+    //     const slippageFactorSushiswap = amountOutSushiswapBigNumber.mul(100 - (slippageTolerance * 100)).div(100);
         
-        const effectiveAmountOutUniswap = slippageFactorUniswap.sub(estimatedGasFeeUniswap);
-        const effectiveAmountOutSushiswap = slippageFactorSushiswap.sub(estimatedGasFeeSushiswap);
+    //     const effectiveAmountOutUniswap = slippageFactorUniswap.sub(gasCostUniswap);
+    //     const effectiveAmountOutSushiswap = slippageFactorSushiswap.sub(gasCostSushiswap);
 
-        const potentialProfitSushiswap = effectiveAmountOutSushiswap.sub(effectiveAmountOutUniswap); // Sushiswap as target
-        const potentialProfitUniswap = effectiveAmountOutUniswap.sub(effectiveAmountOutSushiswap ); // Uniswap as target
+    //     const potentialProfitSushiswap = effectiveAmountOutSushiswap.sub(effectiveAmountOutUniswap); // Sushiswap as target
+    //     const potentialProfitUniswap = effectiveAmountOutUniswap.sub(effectiveAmountOutSushiswap ); // Uniswap as target
 
-        console.log(`Potential Profit on Uniswap: ${ethers.utils.formatEther(potentialProfitUniswap)}`);
-        console.log(`Potential Profit on Sushiswap: ${ethers.utils.formatEther(potentialProfitSushiswap)}`);
+    //     console.log(`Potential Profit on Uniswap: ${ethers.utils.formatEther(potentialProfitUniswap)}`);
+    //     console.log(`Potential Profit on Sushiswap: ${ethers.utils.formatEther(potentialProfitSushiswap)}`);
 
         
 
-    // Determine the most profitable scenario
-    if (potentialProfitSushiswap.gt(0) || potentialProfitUniswap.gt(0)) {
-        if (potentialProfitSushiswap.gt(potentialProfitUniswap)) {
-            console.log(`Profitable Arbitrage Opportunity on Sushiswap: ${ethers.utils.formatEther(potentialProfitSushiswap)}`);
-        } else {
-            console.log(`Profitable Arbitrage Opportunity on Uniswap: ${ethers.utils.formatEther(potentialProfitUniswap)}`);
-        }
-    } else {
-        console.log("No profitable arbitrage opportunity found.");
-    }}catch(e : any){
+    // // Determine the most profitable scenario
+    // if (potentialProfitSushiswap.gt(0) || potentialProfitUniswap.gt(0)) {
+    //     if (potentialProfitSushiswap.gt(potentialProfitUniswap)) {
+    //         console.log(`Profitable Arbitrage Opportunity on Sushiswap: ${ethers.utils.formatEther(potentialProfitSushiswap)}`);
+    //     } else {
+    //         console.log(`Profitable Arbitrage Opportunity on Uniswap: ${ethers.utils.formatEther(potentialProfitUniswap)}`);
+    //     }
+    // } else {
+    //     console.log("No profitable arbitrage opportunity found.");
+    // }
+  }catch(e : any){
         console.log("Error in calculating arbitrage profit");
         console.log(e.message);
     }
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import { ethers } from "ethers";
 import { BigNumber } from 'ethers';
 import { calculateArbitrageProfit, fetchLiquidity, fetch_LiquiditySushiswap } from './dexInteractions';
+import { TOKENS } from './constants';
 
 // import { setupBlocknative } from './monitorMempool';
 
@@ -14,11 +15,16 @@ async function main() {
 
     try{
         // setupBlocknative(); listening to mempool
+        const weth = ethers.utils.getAddress(TOKENS.WETH);
+        const dai = ethers.utils.getAddress(TOKENS.DAI);
+        console.log(weth);
+        console.log(dai);
 
          //fetch data from uniswap, check for liquidity // check for arbitrage opportunity
          const uniswapData : any=  await fetchLiquidity('0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', '0x6B175474E89094C44Da98b954EedeAC495271d0F');
          const sushiSwapData: any = await fetch_LiquiditySushiswap('0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', '0x6B175474E89094C44Da98b954EedeAC495271d0F')
-            await calculateArbitrageProfit(uniswapData, sushiSwapData);
+
+            await calculateArbitrageProfit(uniswapData, sushiSwapData, TOKENS.WETH, TOKENS.DAI);
 
     }catch(e: any){
         console.log(e.message);
