1656ab50041e134d063272cbfb26e0135c54456a
Ahaif <haifouli.abdelmoughite@gmail.com>
2024-04-03 00:40:04 +0000

    ganche local setup

--- contracts/ArbitrageExecutor.sol
+++ contracts/ArbitrageExecutor.sol
@@ -12,43 +12,43 @@ interface IUniswapV2Router {
 }
 
 contract ArbitrageExecutor {
-    // address owner;
-    // IUniswapV2Router uniswapRouter;
-    // IUniswapV2Router sushiswapRouter;
+    address owner;
+    IUniswapV2Router uniswapRouter;
+    IUniswapV2Router sushiswapRouter;
 
-    // constructor(address _uniswapRouter, address _sushiswapRouter) {
-    //     owner = msg.sender;
-    //     uniswapRouter = IUniswapV2Router(_uniswapRouter);
-    //     sushiswapRouter = IUniswapV2Router(_sushiswapRouter);
-    // }
+    constructor(address _uniswapRouter, address _sushiswapRouter) {
+        owner = msg.sender;
+        uniswapRouter = IUniswapV2Router(_uniswapRouter);
+        sushiswapRouter = IUniswapV2Router(_sushiswapRouter);
+    }
 
-    // modifier onlyOwner() {
-    //     require(msg.sender == owner, "Only the contract owner can call this function.");
-    //     _;
-    // }
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the contract owner can call this function.");
+        _;
+    }
 
-    // function executeTrade(
-    //     bool isUniswapToSushiswap,
-    //     uint amountIn,
-    //     uint amountOutMin,
-    //     address[] calldata path,
-    //     address to,
-    //     uint deadline
-    // ) external onlyOwner {
-    //     if (isUniswapToSushiswap) {
-    //         // Execute trade from Uniswap to Sushiswap
-    //         uniswapRouter.swapExactTokensForTokens(amountIn, amountOutMin, path, to, deadline);
-    //     } else {
-    //         // Execute trade from Sushiswap to Uniswap
-    //         sushiswapRouter.swapExactTokensForTokens(amountIn, amountOutMin, path, to, deadline);
-    //     }
-    // }
+    function executeTrade(
+        bool isUniswapToSushiswap,
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external onlyOwner {
+        if (isUniswapToSushiswap) {
+            // Execute trade from Uniswap to Sushiswap
+            uniswapRouter.swapExactTokensForTokens(amountIn, amountOutMin, path, to, deadline);
+        } else {
+            // Execute trade from Sushiswap to Uniswap
+            sushiswapRouter.swapExactTokensForTokens(amountIn, amountOutMin, path, to, deadline);
+        }
+    }
 
-    // // Function to allow the contract to receive ETH
-    // receive() external payable {}
+    // Function to allow the contract to receive ETH
+    receive() external payable {}
 
-    // // Withdraw ETH from the contract to the owner address
-    // function withdraw() public onlyOwner {
-    //     payable(owner).transfer(address(this).balance);
-    // }
+    // Withdraw ETH from the contract to the owner address
+    function withdraw() public onlyOwner {
+        payable(owner).transfer(address(this).balance);
+    }
 }
